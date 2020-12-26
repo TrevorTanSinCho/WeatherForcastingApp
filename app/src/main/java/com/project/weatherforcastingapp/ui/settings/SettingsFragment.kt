38d1c8f7efbf4d65cc6ede704b978b2888b641a8
TrevorTanSinCho <xingzhu89@gmail.com>
2020-12-27 00:00:43 +0800

    Initial commit

---
+++ app/src/main/java/com/project/weatherforcastingapp/ui/settings/SettingsFragment.kt
@@ -0,0 +1,7 @@
+package com.project.weatherforcastingapp.ui.settings
+
+import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : Fragment() {
+}
